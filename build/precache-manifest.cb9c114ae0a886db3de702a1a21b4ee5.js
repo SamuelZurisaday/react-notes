83c6f86ee5688faee139e4a85e8762f5c91dbd54
Samuel Zurisaday <samuelzrivera@gmail.com>
2019-09-12 21:32:12 -0500

    Configuraciones finales de app

---
+++ build/precache-manifest.cb9c114ae0a886db3de702a1a21b4ee5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f31148b9c47f75f590092dfdb4ee485",
+    "url": "/SamuelZurisaday/react-notes/index.html"
+  },
+  {
+    "revision": "34f13f6911b654319a85",
+    "url": "/SamuelZurisaday/react-notes/static/css/main.a32141f1.chunk.css"
+  },
+  {
+    "revision": "655673e2d6f9bcf9a8e0",
+    "url": "/SamuelZurisaday/react-notes/static/js/2.c394ce9f.chunk.js"
+  },
+  {
+    "revision": "34f13f6911b654319a85",
+    "url": "/SamuelZurisaday/react-notes/static/js/main.98d31402.chunk.js"
+  },
+  {
+    "revision": "a37295573d3b84e13607",
+    "url": "/SamuelZurisaday/react-notes/static/js/runtime~main.029f00fb.js"
+  }
+]);
